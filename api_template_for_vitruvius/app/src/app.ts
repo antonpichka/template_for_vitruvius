c8e6f37d342344073daf14cc33c8ed1efe96ec2d
antonpichka <antonpichka@gmail.com>
2025-01-10 14:57:38 +0200

    refactoring(.): release v2.0.6

--- api_template_for_vitruvius/app/src/app.ts
+++ api_template_for_vitruvius/app/src/app.ts
@@ -3,8 +3,8 @@ import cors from "cors"
 import ip from "ip";
 import express, { Application } from "express";
 import dotenv from "dotenv";
-import routesTypeGet from "./routes_type_get";
-import routesTypePost from "./routes_type_post";
+import routersTypeGet from "./routers_type_get";
+import routersTypePost from "./routers_type_post";
 
 dotenv.config({ path: "../.env" });
 
@@ -14,8 +14,8 @@ const port: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT)
 application.use(cors())
 application.use(express.json());
 application.use(express.urlencoded({ extended: true }));
-application.use("/", routesTypeGet);
-application.use("/", routesTypePost);
+application.use("/", routersTypeGet);
+application.use("/", routersTypePost);
 application.listen(port, () => {
     debugPrint(`Server running on: ${ip.address()}:${port}`);
 });
